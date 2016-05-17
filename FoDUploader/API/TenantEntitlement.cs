57f0ebc0dc10d9362b3741bc8e437ef220a8e6af
Ryan Black <ryanblack@gmail.com>
2016-05-17 15:21:10 -0500

    Modfied help information

--- FoDUploader/API/TenantEntitlement.cs
+++ FoDUploader/API/TenantEntitlement.cs
@@ -1,5 +1,5 @@
 ﻿#region copyright
-// Copyright (c) 2016 -  Ryan Black - ryanblack@gmail.com
+// Copyright (c) 2016 -  HPE Security Fortify on Demand, Ryan Black
 
 //Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -10,9 +10,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FoDUploader.API
 {
